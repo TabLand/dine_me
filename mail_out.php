dfdc6e8127ed69b98cdfe9bb0cfae7b38255fa96
Ijtaba Hussain <ijtabahussain@live.com>
2015-11-15 12:45:38 +0000

    Added mailer

---
+++ mail_out.php
@@ -0,0 +1,56 @@
+<?php 
+    require "functions.php";
+    require "mailer/class.phpmailer.php";
+    require "mailer/class.phpmailer.php";
+    require "secrets.php";
+
+    function verify_captcha(){
+        $ip      = $_SERVER["REMOTE_ADDR"];
+        $url     = 'https://www.google.com/recaptcha/api/siteverify';
+        $data    = array('secret' => GOOGLE_RECAPTCHA , 'response' => $_POST['g-recaptcha-response'], 'remoteip' => $ip);
+        $options = array(
+                    'http' => array(
+                                     'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                                     'method'  => 'POST',
+                                     'content' => http_build_query($data),
+                                    )
+                         );
+        $context  = stream_context_create($options);
+        $result = json_decode(file_get_contents($url, false, $context));
+        var_dump($result);
+    }
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="dine.css?v=0.1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script src='https://www.google.com/recaptcha/api.js'></script>
+    </head>
+	<body id="plate">
+        <div id="text">
+            <?php
+                    $who         = name();
+                    $where       = place();
+                    $when        = when();
+                    $duration    = duration();
+                    $email       = email();
+                    $why         = reason();
+
+                    $mail_text = "Who:   $who\r\n" .
+                                 "Where: $where\r\n" .
+                                 "When:  $when\r\n" .
+                                 "Duration: $duration\r\n" .
+                                 "Email: $email\r\n" .
+                                 "Reason: $why\r\n";
+
+                    if(verify_captcha()){
+                        send_mail($mail_text,$email);
+                    }
+
+            ?>
+        </div>
+    </body>
+</html>
