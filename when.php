41a7e24db3d37cd61819582cf73c05a47236b576
Ijtaba Hussain <ijtabahussain@live.com>
2018-07-25 00:40:56 +0100

    Better label

--- when.php
+++ when.php
@@ -21,7 +21,7 @@
         <div id="text">
             <form method="get" action="validate_when.php">
                 Hmm, not really believable. But, whatever. What date and time are we meeting up?<br/>
-                <input type="text" name="when" placeholder="Date and time"><br/>
+                <input type="text" name="when" placeholder="Datetime, e.g Tomorrow 1pm"><br/>
                 For <input style="width: 100px;" type="number" name="duration" onchange="update_hour()" onkeyup="update_hour()" onkeydown="update_hour()" placeholder="1">
                 <span id="hours">hour</span> 
                 <input type="hidden" name="why" value="<?php echo reason(); ?>"/>
