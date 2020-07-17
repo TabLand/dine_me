8c261079a2fb67040f85e241c87dc7e5c36960bd
Ijtaba Hussain <ijtabahussain@live.com>
2020-07-17 05:51:39 +0100

    A really cool circular window over the plate, better 'accelerated' animations pending...

--- dine.js
+++ dine.js
@@ -1,3 +1,5 @@
+enable_animation = true;
+
 function proceed_with_sound(){
   document.getElementById("audio").play(); 
 }
@@ -6,7 +8,18 @@ function proceed_without_sound(){
   document.getElementById("audio").pause(); 
 }
 
-function animate_mask(){
-  circle = document.getElementById("circle_mask");
-  setInterval(function () {circle.setAttribute("cy", 50 + window.scrollY);}, 5);
+function setup(){
+  text_div = document.getElementById("text");
+  animate_mask(text_div);
+}
+
+function animate_mask(text_div){
+  setInterval(
+    function() {
+      if(enable_animation){
+        new_clip = "circle(300px at 300px " + ((window.innerHeight/2) - ((window.innerHeight - 600) /2) + window.scrollY) + "px";
+        text_div.style.clipPath = new_clip;
+        text_div.style["margin-top"] = ((window.innerHeight - 600) /2);
+      }
+    } , 5);
 }
